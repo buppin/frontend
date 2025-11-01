0e922b96a192b4ef9d48991ed98efe9945ba179d
yohiharu <s2210653@u.tsukuba.ac.jp>
2025-11-01 12:33:21 +0900

    Change to Hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './styles/tailwind.css'
 import "./styles/App.scss"
 import Home from './pages/Home'
@@ -6,12 +6,14 @@ import Home from './pages/Home'
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home></Home>}>
           </Route>
+          <Route path="/a" element={<Home></Home>}>
+          </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
