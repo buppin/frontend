3e43fc31af29c3d3127518704dc8b360c629b1c1
yohiharu <s2210653@u.tsukuba.ac.jp>
2025-11-01 12:21:16 +0900

    Debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import './styles/tailwind.css'
+import "./styles/App.scss"
 import Home from './pages/Home'
 
 function App() {
