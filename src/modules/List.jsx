6fd597e7e58f5c9c113792689b8afdf3cbd15e24
yohiharu <s2210653@u.tsukuba.ac.jp>
2025-11-08 23:41:43 +0900

    Delete no-need-func

--- src/modules/List.jsx
+++ src/modules/List.jsx
@@ -11,9 +11,6 @@ function List({flag, setFlag}) {
     });
     console.log(flag)
   }, [flag])
-  var deleteClick = () => {
-      fetch(`https://api.yohiharu.com/cgi/delete.cgi?id=${item.id}`)
-  }
   return (
     <>
         <div className="rounded-xl flex flex-col w-3/4 p-4 mt-2 mx-auto bg-amber-300">
